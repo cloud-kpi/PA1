4c35048520c8985f3f8c2736c6aac6c0dc780134
Abdul Khok <110694486+cloud-kpi@users.noreply.github.com>
2024-12-30 00:44:26 +0200

    PA3 files added

--- Model.js
+++ Model.js
@@ -1,46 +1,67 @@
+// Initialize granularity values
 let granularityU = parseInt(document.getElementById("granularityU").value);
 let granularityV = parseInt(document.getElementById("granularityV").value);
 
+// Update granularity when sliders are adjusted
 document.getElementById("granularityU").addEventListener("input", function () {
-	granularityU = parseInt(this.value);
-	redraw();
+    granularityU = parseInt(this.value);
+	console.log("U: " + granularityU + "; V: " + granularityV);
+	
+    redraw();
 });
 
 document.getElementById("granularityV").addEventListener("input", function () {
-	granularityV = parseInt(this.value);
-	redraw();
+    granularityV = parseInt(this.value);
+	console.log("U: " + granularityU + "; V: " + granularityV);
+    redraw();
 });
 
 function deg2rad(angle) {
-	return angle * Math.PI / 180;
+    return angle * Math.PI / 180;
 }
 
-function Vertex(p) {
-	this.p = p;
-	this.normal = [];
-	this.triangles = [];
+function Vertex(p, t)
+{
+    this.p = p;
+    this.t = t;
+    this.normal = [];
+    this.triangles = [];
 }
 
-function Triangle(v0, v1, v2) {
-	this.v0 = v0;
-	this.v1 = v1;
-	this.v2 = v2;
-	this.normal = [];
-	this.tangent = [];
+function Triangle(v0, v1, v2)
+{
+    this.v0 = v0;
+    this.v1 = v1;
+    this.v2 = v2;
+    this.normal = [];
+    this.tangent = [];
 }
 
 // Constructor
 function Model(name) {
-	this.name = name;
-	this.iVertexBuffer = gl.createBuffer();
-	this.iIndexBuffer = gl.createBuffer();
-	this.count = 0;
+    this.name = name;
+    this.iVertexBuffer = gl.createBuffer();
+    this.iTexCoordsBuffer = gl.createBuffer();
+    this.iIndexBuffer = gl.createBuffer();
+    this.count = 0;
 
-	this.BufferData = function (vertices, normals, triangles) {
+    this.BufferData = function(vertices, normals, triangles, texcoords, tangents) {
+		console.log("Vertices: " + vertices.length);
+		console.log("Normals: " + normals.length);
+		console.log("Tangents: " + tangents.length);
+		console.log("triangles: " + triangles.length);
+		console.log("Texcoords: " + texcoords.length);
+		
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STREAM_DRAW);
 		gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.iTexCoordsBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, texcoords, gl.STATIC_DRAW);
+
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, triangles, gl.STREAM_DRAW);
 
 		let normalBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
@@ -48,31 +69,63 @@ function Model(name) {
 		gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(shProgram.iAttribNormal);
 
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, triangles, gl.STREAM_DRAW);
-
+		let tangentBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, tangentBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, tangents, gl.STREAM_DRAW);
+		gl.vertexAttribPointer(shProgram.iAttribTangent, 3, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(shProgram.iAttribTangent);
+		
 		this.count = triangles.length;
 	};
 
-	this.Draw = function () {
-		gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
-	}
+    this.Draw = function() {
+
+		gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this.idTextureDiffuse);
+
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, this.idTextureSpecular);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iTexCoordsBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribTexCoords, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribTexCoords);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+
+        //gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+        gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
+    }
 }
 
+// Update the sliders to control granularity
+function redraw() {
+    let data = {};
+    CreateSurfaceData(data);
+    surface.BufferData(data.verticesF32, data.normalsF32, data.trianglesU16, data.texcoordsF32, data.tangentsF32);
+    draw();
+}
+
+
 function CreateSurfaceData(data) {
-	let vertices = [];
-	let normals = [];
-	let triangles = [];
-
+    let vertices = [];
+    let texcoords = [];
+    let normals = [];
+    let triangles = [];
+	
 	let scale = 0.5;
-	const PI = Math.PI;
+    const PI = Math.PI;
 
-	function calc(x, y, z, uTangent, vTangent) {
+	function calc(x, y, z, uTangent, vTangent, u, v) {
 		x *= scale;
 		y *= scale;
 		z *= scale;
 
 		vertices.push(x, y, z);
+		texcoords.push(u/3 + 0.5, (1 - v)/3 + 0.2);
 
 		let nx = uTangent[1] * vTangent[2] - uTangent[2] * vTangent[1];
 		let ny = uTangent[2] * vTangent[0] - uTangent[0] * vTangent[2];
@@ -91,24 +144,24 @@ function CreateSurfaceData(data) {
 	}
 
 	let row = 0;
-	for (let j = -PI / 2; j <= PI / 2; j += PI / granularityV, row++) {
+	for (let j = -PI/2; j <= PI/2; j+=PI/granularityV, row++) {
 		let col = 0;
-		let v = j;
-		for (let i = -PI / 2; i <= PI / 2; i += PI / granularityU, col++) {
-			let u = i;
-			// Bell Pepper
-			// let x = Math.cos(u) * Math.sin(v);
-			// let y = Math.sin(u) * Math.sin(v);
-			// let z = Math.cos(v) + 0.5 * Math.cos(3 * u) * Math.sin(3 * v);
-
+        let v = j;
+        for (let i = -PI/2; i <= PI/2; i+=PI/granularityU, col++) {
+            let u = i;
+            // Bell Pepper
+            // let x = Math.cos(u) * Math.sin(v);
+            // let y = Math.sin(u) * Math.sin(v);
+            // let z = Math.cos(v) + 0.5 * Math.cos(3 * u) * Math.sin(3 * v);
+			
 			// Neovius surface
 			let x = u;
-			let y = v;
-			let z = Math.acos(-3 * (Math.cos(u) + Math.cos(v)) / (3 + 4 * Math.cos(u) * Math.cos(v)));
+            let y = v;
+            let z = Math.acos(-3 * (Math.cos(u) + Math.cos(v)) / (3 + 4 * Math.cos(u) * Math.cos(v)));
 
-
-			let tangentU = [1, 0, 0];
-			let tangentV = [0, 1, -Math.sin(v)];
+			
+			let tangentU = [ 1, 0, 0 ];
+			let tangentV = [ 0, 1, -Math.sin(v) ];
 
 			// Tangent Normalization
 			let lengthTangentU = Math.sqrt(tangentU[0] * tangentU[0] + tangentU[1] * tangentU[1] + tangentU[2] * tangentU[2]);
@@ -118,18 +171,18 @@ function CreateSurfaceData(data) {
 			tangentU = [tangentU[0] / lengthTangentU, tangentU[1] / lengthTangentU, tangentU[2] / lengthTangentU];
 			tangentV = [tangentV[0] / lengthTangentV, tangentV[1] / lengthTangentV, tangentV[2] / lengthTangentV];
 
-
-			calc(x, y, z, tangentU, tangentV); // front
-			// calc(x, y, -z,  tangentU, tangentV); // back
-			// calc(x, z, y,  tangentU, tangentV); // top
-			//calc(x, -z, y,  tangentU, tangentV); // bot
-			//calc(z, x, y,  tangentU, tangentV); // right
-			//calc(-z, x, y,  tangentU, tangentV); // left
-
+			
+			calc(x, y, z, tangentU, tangentV, u, v); // front
+			//calc(x, y, -z); // back
+			//calc(x, z, y); // top
+			//calc(x, -z, y); // bot
+			//calc(z, x, y); // right
+			//calc(-z, x, y); // left
+			
 			push_triangles(row, col);
-		}
-	}
-
+        }
+    }
+	
 	function push_triangles(row, col) {
 		if (row < granularityV && col < granularityU) {
 			let v0 = row * (granularityU + 1) + col;
@@ -141,15 +194,50 @@ function CreateSurfaceData(data) {
 			triangles.push(v1, v3, v2);
 		}
 	}
+	
+	const tangents = calculateTangents(vertices, normals, texcoords, triangles);
 
-	data.verticesF32 = new Float32Array(vertices);
-	data.normalsF32 = new Float32Array(normals);
+    data.verticesF32 = new Float32Array(vertices);
+    data.texcoordsF32 = new Float32Array(texcoords);
+    data.normalsF32 = new Float32Array(normals);
+    data.tangentsF32 = new Float32Array(tangents);
 	data.trianglesU16 = new Uint16Array(triangles);
 }
 
-function redraw() {
-	let data = {};
-	CreateSurfaceData(data);
-	surface.BufferData(data.verticesF32, data.normalsF32, data.trianglesU16);
-	draw();
+function calculateTangents(vertices, normals, texcoords, triangles) {
+    const tangents = new Array(vertices.length / 3).fill(0).map(() => [0, 0, 0]);
+
+    for (let i = 0; i < triangles.length; i += 3) {
+        const i0 = triangles[i];
+        const i1 = triangles[i + 1];
+        const i2 = triangles[i + 2];
+
+        const p0 = vertices.slice(i0 * 3, i0 * 3 + 3);
+        const p1 = vertices.slice(i1 * 3, i1 * 3 + 3);
+        const p2 = vertices.slice(i2 * 3, i2 * 3 + 3);
+
+        const uv0 = texcoords.slice(i0 * 2, i0 * 2 + 2);
+        const uv1 = texcoords.slice(i1 * 2, i1 * 2 + 2);
+        const uv2 = texcoords.slice(i2 * 2, i2 * 2 + 2);
+
+        const edge1 = p1.map((v, j) => v - p0[j]);
+        const edge2 = p2.map((v, j) => v - p0[j]);
+
+        const deltaUV1 = uv1.map((v, j) => v - uv0[j]);
+        const deltaUV2 = uv2.map((v, j) => v - uv0[j]);
+
+        const f = 1.0 / (deltaUV1[0] * deltaUV2[1] - deltaUV1[1] * deltaUV2[0]);
+        const tangent = [
+            f * (deltaUV2[1] * edge1[0] - deltaUV1[1] * edge2[0]),
+            f * (deltaUV2[1] * edge1[1] - deltaUV1[1] * edge2[1]),
+            f * (deltaUV2[1] * edge1[2] - deltaUV1[1] * edge2[2]),
+        ];
+
+        [i0, i1, i2].forEach((index) => {
+            tangents[index] = tangents[index].map((v, j) => v + tangent[j]);
+        });
+    }
+
+    // Normalize tangents
+    return tangents.flat().map((v, i) => v / Math.sqrt(tangents[Math.floor(i / 3)].reduce((acc, val) => acc + val ** 2, 0)));
 }
--- main.js
+++ main.js
@@ -4,23 +4,25 @@ let gl;                         // The webgl context.
 let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let testing = true;
 
 function init() {
     let canvas;
     try {
         canvas = document.getElementById("webglcanvas");
         gl = canvas.getContext("webgl");
-        if (!gl) {
+        if ( ! gl ) {
             throw "Browser does not support WebGL";
         }
     }
     catch (e) {
         document.getElementById("canvas-holder").innerHTML =
             "<p>Sorry, could not get a WebGL graphics context.</p>";
+		console.log(e.message);
         return;
     }
     try {
-        spaceball = new TrackballRotator(canvas, draw, 0);
+		spaceball = new TrackballRotator(canvas, draw, 0);
         initGL();  // initialize the WebGL graphics context
     }
     catch (e) {
@@ -32,95 +34,132 @@ function init() {
     draw();
 }
 
-function ShaderProgram(name, program) {
-    this.name = name;
-    this.prog = program;
-
-    this.iAttribVertex = -1;
-    this.iColor = -1;
-    this.iModelViewProjectionMatrix = -1;
-
-    this.Use = function () {
-        gl.useProgram(this.prog);
-    }
-}
-
-function draw() {
-    gl.clearColor(0, 0, 0, 1);
+function draw() { 
+    gl.clearColor(0,0,0,1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
-
+    
+    // VIEW
+    let projection = m4.perspective(Math.PI/8, 1, 8, 12);  
     let modelView = spaceball.getViewMatrix();
-    let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
-    let translateToPointZero = m4.translation(0, 0, -10);
+    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
+    let translateToPointZero = m4.translation(0,0,-10);
 
     let matAccum0 = m4.multiply(rotateToPointZero, modelView);
     let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
-
+	
     let modelViewProjection = m4.multiply(projection, matAccum1);
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
+    gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, matAccum1);
+
+    // NORMALS
     let normalMatrix = m4.identity();
     m4.multiply(modelView, matAccum1, normalMatrix);
-    m4.inverse(normalMatrix, normalMatrix);
+    m4.inverse(normalMatrix, normalMatrix);    
     m4.transpose(normalMatrix, normalMatrix);
-
     gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
 
-    gl.uniform4fv(shProgram.iColor, [0.7, 0, 0.7, 1]);
+    // COLOR AND TEXTURE
+    gl.uniform4fv(shProgram.iColor, [1, 1, 1, 1]);
 
+    // Bind textures to texture units
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, surface.idTextureDiffuse);  // Diffuse map
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, surface.idTextureSpecular); // Specular map
+
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, surface.idTextureNormal);   // Normal map
+
+    // Draw the surface
     surface.Draw();
 }
 
+
+/* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
+    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
 
     shProgram = new ShaderProgram('Basic', prog);
     shProgram.Use();
-
-    shProgram.iAttribVertex                 = gl.getAttribLocation(prog, "vertex");
-    shProgram.iModelViewProjectionMatrix    = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-    shProgram.iColor                        = gl.getUniformLocation(prog, "color");
-
+	
+    shProgram.iAttribVertex              	= gl.getAttribLocation(prog, "vertex");
+    shProgram.iColor                     	= gl.getUniformLocation(prog, "color");
     shProgram.iAttribNormal                 = gl.getAttribLocation(prog, "normal");
+    shProgram.iAttribTangent                = gl.getAttribLocation(prog, "tangent");
+	
+	shProgram.iAttribTexCoords              = gl.getAttribLocation(prog, "tex");
+	shProgram.iTMU0                         = gl.getUniformLocation(prog, "iTMU0");
+    shProgram.iTMU1                         = gl.getUniformLocation(prog, "iTMU1");
+    shProgram.iTMU2                         = gl.getUniformLocation(prog, "iTMU2");
+
     shProgram.iUniformLightPosition         = gl.getUniformLocation(prog, "lightPosition");
     shProgram.iUniformAmbientLight          = gl.getUniformLocation(prog, "ambientLight");
-
+	
     shProgram.iUniformViewPosition          = gl.getUniformLocation(prog, "viewPosition");
     shProgram.iNormalMatrix                 = gl.getUniformLocation(prog, "NormalMatrix");
     shProgram.iModelViewMatrix              = gl.getUniformLocation(prog, "ModelViewMatrix");
+    shProgram.iModelViewProjectionMatrix 	= gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
 
     gl.uniform3fv(shProgram.iUniformLightPosition, [2, 22, 8.0]);
-    gl.uniform3fv(shProgram.iUniformAmbientLight, [0.3, 0.7, 0.4]);
+    gl.uniform3fv(shProgram.iUniformAmbientLight, [0.3, 0.1, 0.4]);
     gl.uniform3fv(shProgram.iUniformViewPosition, [0, 0, 10.0]);
 
     let data = {};
+    
     CreateSurfaceData(data);
+
     surface = new Model('Surface');
-    surface.BufferData(data.verticesF32, data.normalsF32, data.trianglesU16);
+    surface.BufferData(data.verticesF32, data.normalsF32, data.trianglesU16, data.texcoordsF32, data.tangentsF32);
+
+	surface.idTextureDiffuse  = LoadTexture("tex_diff.png");
+    surface.idTextureSpecular  = LoadTexture("tex_spec.png");
+    surface.idTextureNormal  = LoadTexture("tex_norm.png");
+
     gl.enable(gl.DEPTH_TEST);
 }
 
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vsh, vShader);
+    let vsh = gl.createShader( gl.VERTEX_SHADER );
+    gl.shaderSource(vsh,vShader);
     gl.compileShader(vsh);
-    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
+    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
         throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-    }
-    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
+     }
+    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
-    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
-        throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
+       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
-    gl.attachShader(prog, vsh);
+    gl.attachShader(prog,vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
-    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-        throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
+       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
 
+function ShaderProgram(name, program) {
+
+    this.name = name;
+    this.prog = program;
+
+
+    this.iAttribVertex = -1;
+	this.iAttribTexCoords = -1;
+    this.iColor = -1;
+    this.iModelViewProjectionMatrix = -1;
+	
+	this.iModelViewMatrix = -1;
+    this.iTMU0 = gl.getUniformLocation(program, "iTMU0");
+	this.iTMU1 = gl.getUniformLocation(program, "iTMU1");
+	this.iTMU2 = gl.getUniformLocation(program, "iTMU2");
+
+
+    this.Use = function() {
+        gl.useProgram(this.prog);
+    }
+}
